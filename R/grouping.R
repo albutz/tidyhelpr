def48adf1d283c0cd331f44ff74517b43fdadfa8
Alexander Butz <alex.butz@gmail.com>
2022-01-17 20:00:21 +0100

    docs(grouping.R): add documentation for split_groups

--- R/grouping.R
+++ R/grouping.R
@@ -1,3 +1,20 @@
+#' Split by group and name by group key
+#'
+#' This wrapper function performs a group split via
+#' \code{\link[dplyr]{group_split}} and renames the resulting list elements
+#' with the group keys.
+#'
+#' @param .df A \code{\link[tibble]{tibble}}.
+#' @param .group_vars The column names to group by.
+#'
+#' @return A list of \code{\link[tibble]{tibble}}s split by group named by keys.
+#' @export
+#'
+#' @examples
+#' split_groups(
+#'   datasets::iris,
+#'   "Species"
+#' )
 split_groups <- function(.df, .group_vars) {
   .df_grouped <- .df
 
