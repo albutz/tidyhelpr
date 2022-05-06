969914af03ccb9f8513d91d5175820bd64ff86c5
Alexander Butz <alex.butz@gmail.com>
2022-05-06 19:45:45 +0200

    refactor(random_tibble): Use lorem ipsum for character columns

--- R/random_tibble.R
+++ R/random_tibble.R
@@ -1,3 +1,21 @@
+#' Create a random \code{\link[tibble]{tibble}}
+#'
+#' @param .col_names Column names for the random \code{\link[tibble]{tibble}}.
+#' @param .clean_col_names Should column names be cleaned? Defaults to
+#'   \code{TRUE}.
+#' @param .col_types A list of column types. If \code{NULL} (the default),
+#'   then all columns will be of type \code{character}.
+#' @param .n_rows Number of rows for the random \code{\link[tibble]{tibble}}.
+#'
+#' @return A randomly created \code{\link[tibble]{tibble}} with specified column
+#'   types.
+#' @export
+#'
+#' @examples
+#' random_tibble(
+#'   c("x_1", "X 2"),
+#'   .n_rows = 10
+#' )
 random_tibble <- function(.col_names,
                           .clean_col_names = TRUE,
                           .col_types = NULL,
@@ -12,11 +30,11 @@ random_tibble <- function(.col_names,
       .col_names,
       function(.col) {
         tibble::tibble(
-          purrr::map_chr(
-            seq_len(n_rows),
-            # Sample 10 letters for each column
-            ~ paste(sample(union(letters, LETTERS), 10), collapse = "")
-          )
+          stringi::stri_rand_lipsum(1) %>%
+            stringr::str_split(pattern = " ") %>%
+            purrr::flatten_chr() %>%
+            stringr::str_subset(pattern = "[^A-Za-z]", negate = TRUE) %>%
+            sample(size = .n_rows, replace = TRUE)
         ) %>%
           rlang::set_names(.col)
       }
